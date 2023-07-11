e8b4c8cb1d619feef1afbeb3f9fe9208716fe492
heriksgr <101599899+heriksgr@users.noreply.github.com>
2023-07-11 11:03:47 -0300

    Add files via upload
    plot_heriks_11_07

--- plotaula_11_07.R
+++ plotaula_11_07.R
@@ -1,5 +1,5 @@
 #01 - instalar pacote
-install.packages(ggplot2)
+install.packages("ggplot2")
 
 #02 - carregar pacote
 library(ggplot2)
@@ -9,10 +9,10 @@ dtaula <- read.csv2("dtaula1.csv", stringsAsFactors = T)
 
 #04 - plot, FEVE, marcador
 ggplot(dtaula) +
-  geom_point(aes(x = FEVE, y = MYH7), color = 'blue')
+  geom_point(aes(x = FEVE, y = marcador), color = 'blue')
 
 #05 - plot FEVE, marcador, estagios da doenca
 dtaula$Stage <- factor(dtaula$Stage)
 
 ggplot(dtaula) +
-  geom_point(aes(x = FEVE, y = MYH7, color = Stage))
+  geom_point(aes(x = FEVE, y = marcador, color = Stage))
