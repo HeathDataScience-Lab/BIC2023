87293a9d7146f38a2cef34a1d24aa2da5bc91c4b
adluchessi <andre.luchessi@ufrn.br>
2023-07-13 10:03:04 -0300

    update

--- giovani.R
+++ giovani.R
@@ -1,7 +1,10 @@
+
+library(ggplot2)
+
 ggplot(iris, aes(iris$Sepal.Width, iris$Sepal.Length,
                  shape = Species, color = Species, fill = Species )) +
-  geom_point(size = 3 ) +
-  geom_smooth(method = "lm", se = FALSE) +
+  geom_point(size = 3) +
+  geom_smooth(method = "lm") +
   theme_classic() +
   labs(x = "Largura (cm)", y = "Comprimento (cm)" , title = "Proporções das Espécies") +
   theme(plot.title = element_text(size = 15, colour = "black", face = "bold")) +
@@ -9,5 +12,5 @@ ggplot(iris, aes(iris$Sepal.Width, iris$Sepal.Length,
   theme(axis.text = element_text(size = 10, colour = "black")) +
   theme(axis.text.x = element_text(angle = 20, vjust = .25)) +
   theme(axis.text.y = element_text(angle = 20, vjust = .25)) +
-  scale_x_continuous(breaks = seq(1, 5, 0.5)) +
+  scale_x_continuous(limits = c(1,5), breaks = seq(1, 5, 0.5)) +
   scale_y_continuous(breaks = seq(0, 10, 0.25)) 
