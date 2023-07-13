488a760603983f653477e3d676b1a0ed1cd74201
giovanilavieri <giovanilavieri@hotmail.com>
2023-07-13 10:21:07 -0300

    update

--- giovani.R
+++ giovani.R
@@ -10,7 +10,7 @@ ggplot(iris, aes(iris$Sepal.Width, iris$Sepal.Length,
   theme(plot.title = element_text(size = 15, colour = "black", face = "bold")) +
   theme(axis.title = element_text(size = 12, colour = "black", face = "bold")) +
   theme(axis.text = element_text(size = 10, colour = "black")) +
-  theme(axis.text.x = element_text(angle = 20, vjust = .25)) +
+  theme(axis.text.x = element_text(angle = 12, vjust = .25)) +
   theme(axis.text.y = element_text(angle = 20, vjust = .25)) +
   scale_x_continuous(limits = c(1,5), breaks = seq(1, 5, 0.5)) +
   scale_y_continuous(breaks = seq(0, 10, 0.25)) 
